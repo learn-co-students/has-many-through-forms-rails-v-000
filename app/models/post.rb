9e48b1127460e0339895f7bafba59a0f66b985d0
Brian Holland <brian.holland@proatria.com>
2016-04-01 20:57:34 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :post_categories
 
 
   def categories_attributes=(category_attributes)
+    binding.pry
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.post_categories.build(category: category)
