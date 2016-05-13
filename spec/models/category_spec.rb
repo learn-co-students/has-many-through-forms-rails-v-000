ed74d3156c90f54c0cfbe0e1cefe1b5a54cbd6b1
ShiLi007 <lishidaniel@gmail.com>
2016-05-13 14:26:59 -0400

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a name" do 
+    cat = Category.new
+    cat.name = "Adorable Kittens"
+    expect(cat.name).to eq("Adorable Kittens")
+  end
 end
