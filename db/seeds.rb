f3d6b945524bfbfca97e391dbae82166e1fd3267
McN33ly <rmcneely23@gmail.com>
2016-05-16 19:20:10 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@category = Category.create(name: "Test Posts")
+@post = Post.create(title: "Here we go!!")
+@post_categories = PostCategory.create(category_id: 1, post_id: 1)
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @category = Category.create(name: "Test Posts")
+    @post = Post.create(title: "Here we go!!")
+    @post_categories = PostCategory.create(category_id: 1, post_id: 1)
+  end
+
+  it 'has many through' do
+    expect(@category.post_categories.first.post.title).to eq("Here we go!!")
+  end
+
 end
