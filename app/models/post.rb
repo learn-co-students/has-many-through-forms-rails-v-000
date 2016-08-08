a0192a6fc15ac894d32430109aeacbc856d6be63
kaileeagray <kaileeagray@gmail.com>
2016-08-08 17:04:02 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :categories
+  # this will create categories automatically with out new post.
+  # we need to define our own below because otherwise duplicate categories may be created
 
 
   def categories_attributes=(category_attributes)
