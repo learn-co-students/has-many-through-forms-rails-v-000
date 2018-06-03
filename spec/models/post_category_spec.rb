c8dd33e147ebd3caccebfc996996d7c90a11757b
Carol Greene <carolgreene3@gmail.com>
2018-06-03 20:51:38 +0000

    Done.

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -3,3 +3,5 @@ require 'rails_helper'
 #RSpec.describe PostCategory, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
 #end
+
+#per PJ/Flatiron, I commented out these tests. They're not needed 
