581ae5b773a6ffc20b59799c8c49583e8c96cdc0
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-11 18:09:28 +0000

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @scifi = Category.create(name: "Sci Fi")
+    @post = Post.create(title: "New Post", content: "Post Content")
+    @post.categories << @scifi
+  end
+
+  it 'has a name' do
+    expect(@scifi.name).to eq('Sci Fi')
+  end
+
+  it 'has many posts' do
+    expect(@scifi.posts).to include(@post)
+  end
+
 end
--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before do
+    @scifi = Category.create(name: "Sci Fi")
+    @post = Post.create(title: "New Post", content: "Post Content")
+    @post.categories << @scifi
+  end
+
+  it 'has a title' do
+    expect(@post.title).to eq('New Post')
+  end
+
+  it 'has many categories' do
+    expect(@post.categories).to include(@scifi)
+  end
 end
