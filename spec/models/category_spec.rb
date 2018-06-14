8b4a05f063222e624a2480c72ba337f124e56b43
ryanwevans <ryanwevans@icloud.com>
2018-06-13 18:12:24 -0700

    add rspec tests to category_spec.rb and post_category_spec.rb

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @new_category = Category.create(name: "Journal")
+    @new_category.posts << Post.new(title: "Daily Post", content: "This is a post I write daily")
+  end
+
+
+  it "has a title" do
+    expect(Category.last.name).to eq("Journal")
+  end
+
+  it "has a post with a title and content" do
+    expect(@new_category.posts.last.title).to eq("Daily Post")
+    expect(@new_category.posts.last.content).to eq("This is a post I write daily")
+  end
 end
