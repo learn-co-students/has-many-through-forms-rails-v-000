c96f7e00f943abcd0e98b2de162f44310c179352
Emily Jirles <emily.jirles@gmail.com>
2018-06-15 15:26:30 +0000

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   before do
+     post_1 = Post.create(title: "First Post Ever", content: "This is my first post!")
+     post_2 = Post.create(title: "Second Post", content: "Yet another post. Why?")
+     @post_3 = Post.create(title: "Guess what?!", content: "I wrote this spec!")
+
+     @cat_1 = Category.create(name: "Exciting")
+     cat_2 = Category.create(name: "Meh")
+
+     PostCategory.create(post: post_1, category: @cat_1)
+     PostCategory.create(post: post_2, category: cat_2)
+     PostCategory.create(post: @post_3, category: @cat_1)
+   end
+  it "has a name" do
+    expect(@cat_1.name).to eq("Exciting")
+  end
+
+  it 'has many posts through post_categories' do
+    expect(@cat_1.posts).to include(@post_3)
+    expect(@cat_1.posts.count).to eq(2)
+  end
 end
--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    post_1 = Post.create(title: "First Post Ever", content: "This is my first post!")
+    post_2 = Post.create(title: "Second Post", content: "Yet another post. Why?")
+    post_3 = Post.create(title: "Guess what?!", content: "I wrote this spec!")
+
+    cat_1 = Category.create(name: "Exciting")
+    cat_2 = Category.create(name: "Meh")
+
+    pc_1 = PostCategory.create(post: post_1, category: cat_1)
+    @pc_2 = PostCategory.create(post: post_2, category: cat_2)
+    pc_3 = PostCategory.create(post: post_3, category: cat_1)
+  end
+
+  it "has a category_id and a post_id" do
+     expect(@pc_2.category_id).to eq(2)
+     expect(@pc_2.post_id).to eq(2)
+  end
 end
