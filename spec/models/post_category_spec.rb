b765b6192b0f990c8577f5e1565a636dd5e00895
learn <albazaz@gmail.com>
2018-08-09 20:15:08 -0700

    Done.

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#RSpec.describe PostCategory, type: :model do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
+
+#From my understanding, this is a README
