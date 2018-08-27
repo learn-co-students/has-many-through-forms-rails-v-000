705247c0c785ef89f0bda12b84029c07c46762c1
github username <github email address>
2018-08-26 18:34:53 -0700

    Done.

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @category_1 = Category.create
+    @category_2 = Category.create
+    @new_story = Post.create(title: "New Story About That Damn Elephant")
+    @new_story.category_ids = ["1","2"]
+
+
+  end
+
+  it 'has a title and categories' do
+    expect(@new_story.title).to eq("New Story About That Damn Elephant")
+    expect(@new_story.category_ids).to eq([1,2])
+  end
 end
