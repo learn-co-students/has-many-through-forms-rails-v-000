57fe5a73f367708a82a73539fbd1997d7805350a
Ethel C <cofresi.ethel@gmail.com>
2018-10-04 04:13:34 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20160115202458) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
   create_table "post_categories", force: :cascade do |t|
     t.integer  "post_id"
     t.integer  "category_id"
@@ -32,6 +43,13 @@ ActiveRecord::Schema.define(version: 20160115202458) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "This is a Post", content: "This is the content of a post. I am writing stuff to see what it looks like.")
+Post.create(title: "This is another Post", content: "This is the content of a post. I am writing stuff to see what it looks like.")
+Post.create(title: "This is a Third Post", content: "This is the content of a post. I am writing stuff to see what it looks like.")
+
+User.create(username: "User1", email: "user1@email.com")
+User.create(username: "User2", email: "user2@email.com")
+User.create(username: "User3", email: "user3@email.com")
+
+Category.create(name: "Education")
+Category.create(name: "Science")
+Category.create(name: "Cooking")
+
+PostCategory.create(post_id: 1, category_id: 1)
+PostCategory.create(post_id: 2, category_id: 2)
+PostCategory.create(post_id:3, category_id:3)
+
+Comment.create(user_id: 1, post_id: 2, content: "Great post!")
+Comment.create(user_id: 2, post_id: 3, content: "Awesome job!")
+Comment.create(user_id: 3, post_id: 1, content: "You rock!")
