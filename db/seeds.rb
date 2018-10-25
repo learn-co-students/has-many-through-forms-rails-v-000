1f08e44a4174d72cacfcd15efb4c9c6879ce6a2a
Kayla Cross <kac288@georgetown.edu>
2018-10-25 13:23:21 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+post = Post.create(title: "Malone", content: "Whatever")
+Category.create(name: "Rappers", posts: [post])
