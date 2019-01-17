93c944fd2b3b37f4f65c8546a93a678271d6496e
github username <github email address>
2019-01-17 14:57:56 -0600

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @humor = Category.create(name: "Humor")
+    @drama = Category.create(name: "Drama")
+    @suspense = Category.create(name: "suspense")
+  end
+
+  it 'has a name' do
+    expect(@humor.name).to eq("Humor")
+  end
+
 end
--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @humor = Category.create(name: "Humor")
+    @drama = Category.create(name: "Drama")
+    @suspense = Category.create(name: "suspense")
+    @post1 = Post.create(title: "New Post", content: "Mucho content...")
+    @post2 = Post.create(title: "Daily Ponders", content: "Just general musings for the day")
+    @humor.post_categories.build(post: @post1).save
+    @humor.post_categories.build(post: @post2).save
+    @post2.post_categories.build(category: @drama).save
+    @post2.post_categories.build(category: @suspense).save
+  end
+
+  it 'has many posts' do
+    expect(Category.find_by(name: @humor.name).posts.size).to eq(2)
+  end
+
+  it 'has many categories' do
+    expect(Post.find_by(title: @post2.title).categories.size).to eq(3)
+  end
+
 end
