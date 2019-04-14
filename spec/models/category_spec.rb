1474721134fcf9427b6074342ce784704b845c5d
Jonathan Luk <jluk87@gmail.com>
2019-04-14 03:40:14 +0000

    Done.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Category' do
+
+  it "can be created" do
+    Category.create(name: "Fiction")
+    expect(Category.find_by(name: "Fiction")).to be_present
+  end
+
+
+
+
+
+
 end
