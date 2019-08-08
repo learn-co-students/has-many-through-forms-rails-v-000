e0380d39f180287a22387ff4a0f3c076c11b7dad
Paul Fell <swellpf@gmail.com>
2019-08-08 14:51:00 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def create
+        user = User.new(user_params)
+        redirect_to user
+    end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:username, :content, category_ids:[], categories_attributes: [:name])
+  end
 end
