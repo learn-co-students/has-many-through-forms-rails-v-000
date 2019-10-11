65505ae2ace2c610228ea23e82f2a6cf3c576935
rgrier104 <rgrier104@gmail.com>
2019-10-11 11:58:36 -1000

    Done.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a title field' do
+    expect(Post.new).to respond_to(:title)
+  end
 end
+
