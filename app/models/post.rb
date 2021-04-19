7b60596f4740b877eb19398199e2a478a5fb0fff
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-04-18 21:32:20 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  # # to better control what is happening, do not use/accept the autmatic method from the declaration below; comment out and create your own method as seen next
+  # accepts_nested_attributes_for :categories
 
-
+  # this custom version handles looking up or creating a new category if not passed as ids; those are handled by the other parm in the strong parms method in the controller
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
